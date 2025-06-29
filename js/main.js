6cd9e5444d00f43b3adc45df8996d71d0b4b10f8
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-29 21:50:58 +0600

    js dom

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+let h2 = document.querySelector('h2')
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+let namta1 = document.querySelector('.namta1')
+let namta2 = document.querySelector('.namta2')
+let namta3 = document.querySelector('.namta3')
+// let namta1 = document.querySelector('.namta1')
+
+
+
+
+
+
